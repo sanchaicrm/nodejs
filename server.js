56fd1058d4799216b27dc6d83ec11d4422c50980
Sanchai Thumrongsrisakul <sanchai.thumrongsrisakul@krungthai-axa.co.th>
2018-02-20 20:25:26 +0700

    edit code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 var http = require('http');
 var handleRequest = function (req, res) {
   res.writeHead(200);
-  res.end('Hello Sho Kubernate!');
+  res.end('Hello Sho Kubernate 2222!');
 };
 var www = http.createServer(handleRequest);
 www.listen(process.env.PORT || 8080);
